7504fc9aa505022279b9912dabe05cf4216f63b2
deepakahirwa <139869030+deepakahirwa@users.noreply.github.com>
2024-09-19 01:20:47 +0530

    now i directly uploading images

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,15 +1,22 @@
-import multer from "multer";
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Configure Multer to use Cloudinary for storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'employees', // Folder in Cloudinary where images will be stored
+    format: async (req, file) => 'png', // You can change this to the desired image format
+    public_id: (req, file) => file.originalname.split('.')[0] + '-' + Date.now(),
   },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.originalname + "_" + uniqueSuffix)},
 });
 
-export const upload = multer({
-  storage: storage
-});
-
+export const upload = multer({ storage: storage });
